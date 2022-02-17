18d3452dd70b1cde53c14127dd812205057c30e9
Rubem <rubem.martinsf@gmail.com>
2022-02-17 00:56:15 -0300

    :package: criando agenda app com JavaFX

---
+++ src/com/rubemmartins/agenda/Main.java
@@ -0,0 +1,85 @@
+package com.rubemmartins.agenda;
+	
+import java.io.IOException;
+import java.time.LocalDate;
+
+import com.rubemmartins.agenda.gui.PessoaController;
+import com.rubemmartins.agenda.model.Pessoa;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	
+	private Stage primaryStage;
+	private BorderPane principal;
+	
+	private ObservableList<Pessoa> dados = FXCollections.observableArrayList();
+	
+	public Main () {
+		dados.add(new Pessoa("Rubem", "Martins", LocalDate.of(1996, 11, 6)));
+		dados.add(new Pessoa("Maria", "Madalena", LocalDate.of(1996, 11, 6)));
+		dados.add(new Pessoa("Nelson", "Mandela", LocalDate.of(1918, 7, 18)));// 18 de julho de 1918
+		dados.add(new Pessoa("Frida", "Kahlo", LocalDate.of(1907, 7, 06))); //6 de julho de 1907
+		dados.add(new Pessoa("Ada", "Lovelace", LocalDate.of(1815, 12, 10))); //10 de dezembro de 1815
+		dados.add(new Pessoa("Karen", "Horney", LocalDate.of(1885, 9, 16))); // 16 de setembro de 1885
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Agenda App");
+		
+		initMainStage();
+		carregarTela();
+	}
+	
+	private void carregarTela() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("gui/principal.fxml"));
+			AnchorPane pessoasView = (AnchorPane) loader.load();
+		
+			this.principal.setCenter(pessoasView);
+		
+			PessoaController controller = loader.getController();
+			controller.setMain(this);
+			} 
+		
+		catch (IOException ex) {
+		  	ex.printStackTrace();
+	    }
+	}
+	
+	private void initMainStage() {
+		try {
+				FXMLLoader loader = new FXMLLoader();
+				loader.setLocation(Main.class.getResource("gui/container.fxml"));
+				this.principal = (BorderPane) loader.load();
+			
+				Scene cena = new Scene(this.principal);
+				this.primaryStage.setScene(cena);
+				this.primaryStage.show();
+			} 
+		
+		catch (IOException ex) {
+				ex.printStackTrace();
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	public ObservableList<Pessoa> getDados() {
+		return this.dados;
+	}
+}
