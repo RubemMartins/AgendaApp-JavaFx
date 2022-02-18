d1f1b60bc4aa8273a85a3a64e04977f88e2910c4
Rubem <rubem.martinsf@gmail.com>
2022-02-18 00:30:23 -0300

    :mouse_three_button: adicionando ações aos botões

--- src/com/rubemmartins/agenda/Main.java
+++ src/com/rubemmartins/agenda/Main.java
@@ -1,8 +1,9 @@
 package com.rubemmartins.agenda;
-	
+
 import java.io.IOException;
 import java.time.LocalDate;
 
+import com.rubemmartins.agenda.gui.EditaPessoaController;
 import com.rubemmartins.agenda.gui.PessoaController;
 import com.rubemmartins.agenda.model.Pessoa;
 
@@ -13,66 +14,94 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
-	
+
 	private Stage primaryStage;
 	private BorderPane principal;
-	
+
 	private ObservableList<Pessoa> dados = FXCollections.observableArrayList();
-	
-	public Main () {
+
+	public Main() {
 		dados.add(new Pessoa("Rubem", "Martins", LocalDate.of(1996, 11, 6)));
 		dados.add(new Pessoa("Maria", "Madalena", LocalDate.of(1996, 11, 6)));
 		dados.add(new Pessoa("Nelson", "Mandela", LocalDate.of(1918, 7, 18)));// 18 de julho de 1918
-		dados.add(new Pessoa("Frida", "Kahlo", LocalDate.of(1907, 7, 06))); //6 de julho de 1907
-		dados.add(new Pessoa("Ada", "Lovelace", LocalDate.of(1815, 12, 10))); //10 de dezembro de 1815
+		dados.add(new Pessoa("Frida", "Kahlo", LocalDate.of(1907, 7, 06))); // 6 de julho de 1907
+		dados.add(new Pessoa("Ada", "Lovelace", LocalDate.of(1815, 12, 10))); // 10 de dezembro de 1815
 		dados.add(new Pessoa("Karen", "Horney", LocalDate.of(1885, 9, 16))); // 16 de setembro de 1885
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Agenda App");
-		
+
 		initMainStage();
 		carregarTela();
 	}
-	
+
 	private void carregarTela() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(Main.class.getResource("gui/principal.fxml"));
 			AnchorPane pessoasView = (AnchorPane) loader.load();
-		
+
 			this.principal.setCenter(pessoasView);
-		
+
 			PessoaController controller = loader.getController();
 			controller.setMain(this);
-			} 
-		
+		}
+
 		catch (IOException ex) {
-		  	ex.printStackTrace();
-	    }
+			ex.printStackTrace();
+		}
 	}
-	
+
 	private void initMainStage() {
 		try {
-				FXMLLoader loader = new FXMLLoader();
-				loader.setLocation(Main.class.getResource("gui/container.fxml"));
-				this.principal = (BorderPane) loader.load();
-			
-				Scene cena = new Scene(this.principal);
-				this.primaryStage.setScene(cena);
-				this.primaryStage.show();
-			} 
-		
-		catch (IOException ex) {
-				ex.printStackTrace();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("gui/container.fxml"));
+			this.principal = (BorderPane) loader.load();
+
+			Scene cena = new Scene(this.principal);
+			this.primaryStage.setScene(cena);
+			this.primaryStage.show();
 		}
-		
+
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	public boolean mostrarContatoDialog(Pessoa pessoa) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("gui/edita.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Edita Pessoa");
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.initOwner(this.primaryStage);
+			
+			Scene scene = new Scene(page);
+			dialogStage.setScene(scene);
+			
+			EditaPessoaController controller = loader.getController();
+			controller.setDialogStage(dialogStage);
+			controller.setPessoa(pessoa);
+			
+			dialogStage.showAndWait();
+			
+			return controller.isOkClicked();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	public static void main(String[] args) {
--- src/com/rubemmartins/agenda/gui/PessoaController.java
+++ src/com/rubemmartins/agenda/gui/PessoaController.java
@@ -5,6 +5,8 @@ import com.rubemmartins.agenda.model.Pessoa;
 import com.rubemmartins.agenda.util.DateUtil;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -55,6 +57,44 @@ public class PessoaController {
 			rotuloSobreNome.setText("");
 			rotuloDataNascimento.setText("");
 		}
+	}
+	
+	@FXML
+	private void handleNovaPessoa() {
+		Pessoa temp = new Pessoa();
+		boolean okClicked = main.mostrarContatoDialog(temp);
+		if(okClicked) {
+			main.getDados().add(temp);
+		}
+	}
+	
+	@FXML
+	private void handleExcluiPessoa() {
+		int selecionado = tabela.getSelectionModel().getSelectedIndex();
+		if (selecionado >= 0) {
+			tabela.getItems().remove(selecionado);
+		} else {
+			mostrarAlerta();
+		}
+	}
+	
+	@FXML
+	private void handleEditaPessoa() {
+		Pessoa selecionada = tabela.getSelectionModel().getSelectedItem();
+		if(selecionada != null) {
+			boolean okClicked = main.mostrarContatoDialog(selecionada);
+			if(okClicked) this.mostraDetalhe(selecionada);
+		}else {
+			mostrarAlerta();
+		}
+	}
+
+	private void mostrarAlerta() {
+		Alert alert = new Alert(AlertType.WARNING);
+		alert.setTitle("Nenhuma seleção");
+		alert.setHeaderText("Nenhuma Pessoa selecionada.");
+		alert.setContentText("Por Favor, seleciona uma pessoa.");
+		alert.showAndWait();
 	}
 
 }
